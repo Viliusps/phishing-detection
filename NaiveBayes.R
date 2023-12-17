3820be14363f57aaa21e6d6588a82b53f59e98e1
Augustinas <augustinas.jukna@gmail.com>
2023-12-17 17:40:35 +0200

    Naive Bayes implementation using h2o

--- NaiveBayes.R
+++ NaiveBayes.R
@@ -4,8 +4,9 @@ library(ggplot2)
 library(h2o)
 library(foreign)
 library(caret)
+library(tidymodels)
 
-setwd("/Users/augustinas/Documents/GitHub/phishing-detection")
+setwd("C:\\Users\\augus\\Documents\\GitHub\\Fork\\phishing-detection")
 
 data <- read.arff("dataSet.arff")
 
@@ -66,6 +67,12 @@ cat("AUC-ROC:", auc, "\n")
 p_value <- 2 * (1 - auc)
 cat("P-value:", p_value, "\n")
 
+y_pred <- as.vector(h2o.predict(best_model, test_pp.h2o)$predict)
+y_true <- test$Result
+y_pred <- as.factor(y_pred)
+y_true <- as.factor(y_true)
+y_prob <- as.vector(h2o.predict(best_model, test_pp.h2o)$p1)
+
 pr_auc <- h2o.aucpr(best_model, train = TRUE, valid = FALSE)
 cat("AUC-PR:", pr_auc, "\n")
 
@@ -82,14 +89,13 @@ cat("F1 Score:", f1_score, "\n")
 fpr <- h2o.performance(best_model, xval = TRUE) %>% h2o.fpr() %>% .[['fpr']]
 tpr <- h2o.performance(best_model, xval = TRUE) %>% h2o.tpr() %>% .[['tpr']]
 auc <- h2o.auc(best_model, xval = TRUE)
-#print(data.frame(fpr = fpr, tpr = tpr))
 data.frame(fpr = fpr, tpr = tpr) %>%
   ggplot(aes(fpr, tpr) ) +
-  geom_line() + 
+  geom_line() +
   ggtitle( sprintf('AUC: %f', auc) ) +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "blue") +
   xlim(c(0, 1)) +
-  ylim(c(0, 1)) 
+  ylim(c(0, 1))
 
 fnr <- 1 - tpr
 data.frame(fpr, fnr) %>%
@@ -99,61 +105,53 @@ data.frame(fpr, fnr) %>%
   xlab("False Positive Rate (FPR)") +
   ylab("False Negative Rate (FNR)")
 
-y_pred <- as.vector(h2o.predict(best_model, test_pp.h2o)$predict)
-y_true <- test$Result
-y_pred <- as.factor(y_pred)
-y_true <- as.factor(y_true)
 conf_matrix <- caret::confusionMatrix(y_true, y_pred)
 
 
+accuracy <- sum(diag(conf_matrix$table)) / sum(conf_matrix$table)
+cat("Accuracy:", accuracy, "\n")
+
+
+
+################### CONFUSION MATRIX ######################
+library(tidyr)
 conf_matrix_df <- as.data.frame.matrix(conf_matrix$table)
-
 conf_matrix_df_norm <- prop.table(as.matrix(conf_matrix_df), margin = 1)
-
 conf_matrix_df_norm <- as.data.frame.matrix(conf_matrix_df_norm)
 conf_matrix_df_norm <- cbind(TClass = rownames(conf_matrix_df_norm), as.data.frame(conf_matrix_df_norm))
-
-library(tidyr)
 df_norm <- pivot_longer(conf_matrix_df_norm, cols = -TClass, names_to = "PClass", values_to = "value")
 
 ggplot(data = df_norm, mapping = aes(x = PClass, y = TClass)) +
   geom_tile(aes(fill = value), colour = "white") +
-  geom_text(aes(label = sprintf("%.2f", value)), vjust = 1) +  
+  geom_text(aes(label = sprintf("%.2f", value)), vjust = 1) +
   scale_fill_gradient(low = "blue", high = "red") +
   theme_bw() + theme(legend.position = "none") +
   labs(title = "Normalized Confusion Matrix", x = "Predicted Class", y = "True Class")
+##########################################################
 
 
-y_prob <- as.vector(h2o.predict(best_model, test_pp.h2o)$p1)
-y_true <- as.factor(y_true)
-y_pred_threshold <- as.factor(y_pred_threshold)
-pr_data <- data.frame(
-  threshold = seq(0, 1, length.out = 100),
-  precision = numeric(100),
-  recall = numeric(100)
-)
+################### PR CURVE ######################
+library(PRROC)
+weights_class0 <- as.numeric(test$Result == 1)
+pr_result_with_curve <- pr.curve( y_prob, weights.class0 = weights_class0, curve = TRUE)
 
-for (i in seq_along(pr_data$threshold)) {
-  threshold <- pr_data$threshold[i]
-  y_pred_threshold <- ifelse(y_prob >= threshold, 1, 0)
-  conf_matrix_threshold <- confusionMatrix(y_true, y_pred_threshold)
-  
-  tp <- conf_matrix_threshold[2, 2]
-  fp <- conf_matrix_threshold[1, 2]
-  fn <- conf_matrix_threshold[2, 1]
-  
-  precision <- tp / (tp + fp)
-  recall <- tp / (tp + fn)
-  
-  pr_data$precision[i] <- precision
-  pr_data$recall[i] <- recall
+plot(pr_result_with_curve$curve, type = 'l', col = 'blue', lwd = 2,
+     xlab = 'Recall', ylab = 'Precision', main = 'Precision-Recall Curve')
+
+####################################################
+
+
+#Permutation importance
+accuracy_before_permutation <- confusionMatrix(y_true, y_pred)$overall["Accuracy"]
+importance_values <- numeric(length(features))
+for (i in seq_along(features)) {
+  permuted_data <- test_pp
+  permuted_data[[features[i]]] <- sample(permuted_data[[features[i]]])
+  y_permuted <- as.factor(h2o.predict(best_model, as.h2o(permuted_data))$predict)
+  accuracy_after_permutation <- confusionMatrix(y_true, y_permuted)$overall["Accuracy"]
+  importance_values[i] <- accuracy_before_permutation - accuracy_after_permutation
 }
+ranked_features <- features[order(importance_values, decreasing = TRUE)]
+print(ranked_features)
 
-ggplot(data = pr_data, aes(x = recall, y = precision)) +
-  geom_line(color = "blue") +  # Specify color for the line
-  geom_point(color = "red", size = 2) +  # Specify color and size for the points
-  ggtitle("Precision-Recall Curve") +
-  xlab("Recall") +
-  ylab("Precision") +
-  scale_color_gradient(low = "blue", high = "red") +  # Adjust the color scale
-  theme_bw()
+h2o.download_pojo(best_model)
